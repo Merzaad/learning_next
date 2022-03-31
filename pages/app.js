ed91d5e0dd611a4fb272150b855102de15ad5ee3
Merzaad <Merzaad@gmail.com>
2022-03-31 17:53:49 +0430

    initial

--- pages/app.js
+++ pages/app.js
@@ -1,6 +1,38 @@
 import * as React from "react"
+import Box from "@mui/material/Box"
+import LoadingButton from '@mui/lab/LoadingButton'
+
+const styles = {
+  loadingButton : {
+    fontSize: '25px',
+    color: '#141E27',
+    background: '#E2D784',
+    transitionDuration: '0.3s',
+    ':hover': {
+      background: '#141E27',
+      color:'#E2D784',
+    }
+  },
+  box: {
+    width: '50%',
+    fontSize:'25px',
+    fontFamily: 'arial',
+    textAlign: "center",
+    border: "1px solid black",
+    borderRadius: "10px",
+    margin: "auto",
+    padding: "20px",
+    background: '#203239',
+    color: 'white',
+    transition: 'width 1s',
+    '@media only screen and (max-width: 600px)':{
+      width: '80%'
+    }
+  },
+}
+
 const App = () => {
-  const [title, setTitle] = React.useState("done")
+  const [title, setTitle] = React.useState("zero")
   const [count, setCount] = React.useState(0)
 
   const delay = () =>
@@ -9,26 +41,34 @@ const App = () => {
     })
 
   const clickHandler = async () => {
-    if (title === "calling") return
-    setTitle("calling")
+    if (title === "counting") return
+    setTitle("counting")
     const res = await delay()
     setCount((prev) => prev + res)
     setTitle("done")
   }
-
-  const btnTxt = title === "done" ? "+" : "..."
 
   React.useEffect(() => {
     console.log("counted")
   }, [count])
 
   return (
-    <div style={{ textAlign: "center" }}>
+    <Box
+      sx={styles.box}
+    >
       <h1>{title}</h1>
       <p>{count}</p>
-      <button onClick={clickHandler}>{btnTxt}</button>
-      <hr />
-    </div>
+       <LoadingButton
+        sx={styles.loadingButton}
+        onClick={clickHandler}
+        loading={title === 'counting'}
+        loadingPosition="center"
+        variant="contained"
+      >
+        +1
+      </LoadingButton>
+    </Box>
   )
 }
 export default App
+
