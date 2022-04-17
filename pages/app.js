4b862f8873773e8ea0d2d735e00a6c98e42a448f
Merzaad <Merzaad@gmail.com>
2022-04-17 11:13:24 +0430

    delayApiMaker

--- pages/app.js
+++ pages/app.js
@@ -1,28 +1,37 @@
 import * as React from "react"
-import fetchingApi from "./api/api"
-//testtttttttttttttt
-const state1 = fetchingApi(3)
-const state2 = fetchingApi(4)
+import delayApiMaker from "./api/api"
+import { CircularProgress } from "@mui/material"
+import styles from "../styles/Home.module.css"
 
-function Data1() {
-  const data = state1.fetch()
-  return <h1>{data}</h1>
+
+const App = () => {
+  //link to closure
+  const api1 = delayApiMaker(2)
+  const api2 = delayApiMaker(4)
+
+  //using closure
+  const Component1 = () => {
+    const data = api1.fetch()
+    return <h1>{data}</h1>
+  }
+
+  const Component2 = () => {
+    const test = () => api2.fetch()
+    const [data, setData] = React.useState(test)
+    console.log(data)
+    return <h1>{data}</h1>
+  }
+  
+  return (
+    <div className={styles.main}>
+      <React.Suspense fallback={<CircularProgress />}>
+        <Component1 />
+        <React.Suspense fallback={<CircularProgress />}>
+          <Component2 />
+        </React.Suspense>
+      </React.Suspense>
+    </div>
+  )
 }
-
-function Data2() {
-  const test = ()=> state2.fetch()
-  const [data, setData] = React.useState(test)
-  console.log(data)
-  return <h1>{data}</h1>
-}
-
-const App = () => (
-  <React.Suspense fallback={<h1>Loading...</h1>}>
-    <Data1 />
-    <React.Suspense fallback={<h1>Loading...</h1>}>
-      <Data2 />
-    </React.Suspense>
-  </React.Suspense>
-)
 
 export default App
